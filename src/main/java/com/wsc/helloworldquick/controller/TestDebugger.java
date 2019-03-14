42c4a7b17101bf0bc89d49b8f17d5a64a81312a1
lamda2019 <569188901@qq.com>
2019-03-15 00:33:17 +0800

    添加

---
+++ src/main/java/com/wsc/helloworldquick/controller/TestDebugger.java
@@ -0,0 +1,24 @@
+package com.wsc.helloworldquick.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestDebugger {
+
+    public static void main(String[] args){
+        List<String> list =new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("ok"+i);
+        }
+        System.out.println("ok"); System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+        System.out.println("ok");
+    }
+}
