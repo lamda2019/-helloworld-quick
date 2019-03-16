ac56bda8c6e7aa098330c6e924a8794be8d36d99
lamda2019 <569188901@qq.com>
2019-03-16 15:43:13 +0800

    ok

---
+++ src/main/java/com/wsc/helloworldquick/ui/UITest.java
@@ -0,0 +1,25 @@
+package com.wsc.helloworldquick.ui;
+
+public class UITest {
+
+    public void main(String[] args){
+        System.out.println("ok");
+
+         test(1,"wsc");
+        System.out.println("版本控制提交项目");
+
+
+    }
+public static void test(int age, String name){
+    System.out.println("");
+}
+
+
+}
+
+
+
+
+
+
+
